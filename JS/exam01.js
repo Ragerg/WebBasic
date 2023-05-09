21844597919bb1d7e4fd9c3ef10a77cc970981c4
Lee Nayeon <in6306174@gmail.com>
2023-05-09 13:06:27 +0900

    자바스크립트 실습 1,2

---
+++ JS/exam01.js
@@ -0,0 +1,13 @@
+function inname() {
+   let name = prompt('이름을 입력해 주세요');
+   check(name);
+}
+
+function check(name) {
+    if(name === '' || name === null) {
+        inname();
+    } else {
+        console.log(name);
+        alert(name + "님 안녕하세요");
+    }
+}
---
+++ JS/utils.js
@@ -0,0 +1,15 @@
+//return X, parameter X
+function hi() {
+    // alert("Hi!!!!!");
+    console.log("Hi!!!!!!!!!!");
+}
+
+
+function sum(x, y) {  //타입을 따로 쓰지 않는다
+    let result = x + y;
+    console.log(result);
+}
+
+function total(x, y) {
+    return x + y;
+}
