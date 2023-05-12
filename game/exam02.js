d647461e210afc09b4faf2a7fb9ee9b8518989d8
Rage <skylark1942@gmail.com>
2023-05-13 03:12:20 +0900

    펭귄장착 ,점수계산, 점프+발사, 시작화면, 게임오버화면 오류 수정...

--- game/exam02.js
+++ game/exam02.js
@@ -3,6 +3,10 @@ $(function () {
   const enemy = $("#enemy");
   const pang = $("#pang");
 
+  let isJumping = false;
+  let score = 0;
+  let isLaunching = false;
+
   //배경 움직이게
   function backgroundSlide() {
     let posi = 0;
@@ -15,11 +19,12 @@ $(function () {
     }, 10);
   }
 
-  // 점프 중인지?
-  let isJumping = false;
-  let score = 0;
-  let isLaunching = false;
+  //시작화면
+  backgroundSlide();
+  square();
+  $("#score").css("display", "none");
 
+  //난이도 선택
   $("#easystart_button").click(function () {
     gameStart("easy");
   });
@@ -28,31 +33,34 @@ $(function () {
     gameStart("hard");
   });
 
-  square();
+  //난이도에 따라 게임시작
   function gameStart(difficulty) {
     let min = 0,
       max = 0;
     switch (difficulty) {
       case "easy":
-        min = 1000;
+        min = 1500;
         max = 3000;
         break;
       case "hard":
-        min = 500;
+        min = 1000;
         max = 2000;
         break;
     }
 
+    //게임시작 시 시작화면 숨김
     $("#gamestart_screen").hide();
     $(".square").hide();
+    $("#score").css("display", "inline-block");
+    $("#canAttack").css("display", "block");
+
     setKeyboardEvent();
     enemyStart(min, max);
 
     checkGameOver();
-    backgroundSlide();
   }
 
-  //캐릭터가 적과 충돌
+  //캐릭터가 적과 충돌했는지
   function isColliding(el1, el2) {
     const rect1 = el1.getBoundingClientRect();
     const rect2 = el2.getBoundingClientRect();
@@ -65,19 +73,25 @@ $(function () {
     );
   }
 
+  //게임오버 체크
   function checkGameOver() {
     setInterval(function () {
+      //충돌하면
       if (isColliding(hero[0], enemy[0])) {
+        //캐릭터, 적, 펭귄 멈추고
         hero.stop();
         enemy.stop();
         pang.stop();
+        //키이벤트 안먹히게 조건 설정
         isJumping = true;
         isLaunching = true;
 
+        //게임오버 화면 출력
         if (!$("#gameover_screen").is(":visible")) {
-          square();
+          $(".square").show();
           $("#gameover_screen").show();
           $("#score").css("display", "none");
+          $("#canAttack").css("display", "none");
         }
       }
     }, 1000 / 60);
@@ -87,15 +101,13 @@ $(function () {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  //게임 오버 시 랜덤하게 박스들 생성
+  //화면에 랜덤하게 박스들 생성
   function square() {
-    // let colors = ["#ff0000", "#0000ff", "#00ff00"];
-    // let colors = ["#c7339b90", "#7ac73390", "#c4d01d1b", "#1d29d01b"];
     let colors = ["#7ac733d8", "#c7339be1", "#c4d01dc5", "#1d29d0da"];
 
     setInterval(function () {
       $(".square").each(function () {
-        let top = getRandomNumber(-200, 400) + "px";
+        let top = getRandomNumber(0, 600) + "px";
         let left = getRandomNumber(-300, 900) + "px";
         let color = colors[Math.floor(Math.random() * colors.length)];
 
@@ -115,11 +127,13 @@ $(function () {
       .stop() // 기존의 애니메이션 중지
       .css({ right: "-70px", display: "block" }) // 초기 위치와 표시 설정
       .animate({ right: "850px" }, speed, "linear", function () {
-        // 점수 올리자
-        score += 100;
-        updateScore(score);
+        if (enemy.css("display") !== "none") {
+          // 점수 올리자
+          score += 100;
+          updateScore(score);
+        }
         //적이 리셋
-        enemyStart();
+        enemyStart(min, max);
       });
   }
 
@@ -130,40 +144,48 @@ $(function () {
 
   function jump() {
     isJumping = true;
+    $("#canAttack").css("display", "none"); // "공격 가능" 숨김
     hero
       .animate({ bottom: "190px" }, 500)
-      .animate({ bottom: "50px" }, 800, function () {
+      .animate({ bottom: "50px" }, 700, function () {
         isJumping = false;
+        $("#canAttack").css("display", "block"); // "공격 가능" 표시
       });
   }
 
   // 펭귄발사!!
   function attack() {
     isLaunching = true;
-    pang
-      .css("display", "block")
-      .animate({ left: "900px" }, 700, "linear")
-      .animate({ left: "100px" }, 0, function () {
-        setTimeout(function () {
-          isLaunching = false;
-        }, 2000);
-        pang.css("display", "none");
+    $("#canAttack").css("display", "none"); // "공격 가능" 숨김
+    pang.css("display", "block").animate({ left: "900px" }, 500, "linear");
+    crushEnemy();
+    pang.animate({ left: "100px" }, 0, function () {
+      setTimeout(function () {
+        isLaunching = false;
+        $("#canAttack").css("display", "block"); // "공격 가능" 표시
+      }, 3000);
+      pang.css("display", "none");
+    });
+  }
 
-        // 점수 올리자
-        if (checkAttack()) {
-          enemy.hide();
-          score += 200;
-          updateScore(score);
-        }
-      });
+  function crushEnemy() {
+    if (checkAttack()) {
+      enemy.css("display", "none");
+      // 점수 올리자
+      score += 200;
+      updateScore(score);
+    }
   }
 
   //펭귄이 적과 충돌헸는지
   function checkAttack() {
     const rect1 = pang[0].getBoundingClientRect();
     const rect2 = enemy[0].getBoundingClientRect();
-    const result = rect1.right < rect2.left;
-    console.log(result);
+    const result = rect1.right <= rect2.left;
+
+    console.log(rect1);
+    console.log(rect2);
+
     return result;
   }
 
@@ -181,7 +203,6 @@ $(function () {
             attack();
           }
       }
-      // console.log(e.key);
     });
   }
 });
