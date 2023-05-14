724b049b586afe484c3ecd3682314949c5b092fa
Rage <skylark1942@gmail.com>
2023-05-15 01:12:14 +0900

    배경음악, 효과음 삽입/펭귄 속도 조절

--- game/game.js
+++ game/game.js
@@ -2,10 +2,13 @@ $(function () {
   const hero = $("#hero");
   const enemy = $("#enemy");
   const pang = $("#pang");
+  const music = new Audio("audios/Spy.mp3");
 
   let isJumping = false;
   let score = 0;
   let isLaunching = false;
+  let isGameOver = false;
+  let attackInterval;
 
   //배경 움직이게
   function backgroundSlide() {
@@ -17,6 +20,17 @@ $(function () {
         posi = 0;
       }
     }, 10);
+  }
+
+  //배경음악 재생
+  function backgroundmusic() {
+    music.loop = true;
+    music.play();
+  }
+
+  // 배경음악 멈추기
+  function stopBackgroundMusic() {
+    music.pause();
   }
 
   //시작화면
@@ -35,6 +49,8 @@ $(function () {
 
   //난이도에 따라 게임시작
   function gameStart(difficulty) {
+    //배경음악 재생
+    backgroundmusic();
     let min = 0,
       max = 0;
     switch (difficulty) {
@@ -55,6 +71,7 @@ $(function () {
     $("#canAttack").css("display", "block");
 
     setKeyboardEvent();
+    setAttackInterval(difficulty); // 공격 가능 간격 설정
     enemyStart(min, max);
 
     checkGameOver();
@@ -73,18 +90,32 @@ $(function () {
     );
   }
 
+  // 게임오버 효과음 재생
+  function playgameOverSound() {
+    const gameOverSound = new Audio("audios/gameOver.wav");
+    gameOverSound.play();
+  }
+
   //게임오버 체크
   function checkGameOver() {
     setInterval(function () {
-      //충돌하면
-      if (isColliding(hero[0], enemy[0])) {
+      // 충돌을 확인하고 이미 게임 오버 상태가 아닌 경우에만 처리
+      if (isColliding(hero[0], enemy[0]) && !isGameOver) {
+        // 게임 오버 상태 플래그 업데이트
+        isGameOver = true;
+
         //캐릭터, 적, 펭귄 멈추고
         hero.stop();
         enemy.stop();
         pang.stop();
+
         //키이벤트 안먹히게 조건 설정
         isJumping = true;
         isLaunching = true;
+
+        // 게임오버 효과음 재생, 배경음악 멈추기
+        playgameOverSound();
+        stopBackgroundMusic();
 
         //게임오버 화면 출력
         if (!$("#gameover_screen").is(":visible")) {
@@ -118,8 +149,15 @@ $(function () {
     }, 800);
   }
 
+  // 적등장 효과음 재생
+  function playEnemySound() {
+    const enemySound = new Audio("audios/enemy.wav");
+    enemySound.play();
+  }
+
   function enemyStart(min, max) {
     // 속도 조절
+    playEnemySound();
     const speed = getRandomNumber(min, max);
 
     // 적이 오른쪽에서 왼쪽으로 이동
@@ -142,6 +180,12 @@ $(function () {
     $("#finalScore").text("Score : " + score);
   }
 
+  // 점프 효과음 재생
+  function playJumpSound() {
+    const jumpSound = new Audio("audios/jump.flac");
+    jumpSound.play();
+  }
+
   function jump() {
     isJumping = true;
     $("#canAttack").css("display", "none"); // "공격 가능" 숨김
@@ -151,19 +195,39 @@ $(function () {
         isJumping = false;
         $("#canAttack").css("display", "block"); // "공격 가능" 표시
       });
+    playJumpSound(); // 점프 효과음 재생
+  }
+
+  // 공격 가능 간격 설정
+  function setAttackInterval(difficulty) {
+    switch (difficulty) {
+      case "easy":
+        attackInterval = 3000; // easy 난이도: 3000ms (3초)
+        break;
+      case "hard":
+        attackInterval = 5000; // hard 난이도: 5000ms (5초)
+        break;
+    }
+  }
+
+  // 펭귄 발사 효과음 재생
+  function playPangSound() {
+    const pangSound = new Audio("audios/pang.wav");
+    pangSound.play();
   }
 
   // 펭귄발사!!
   function attack() {
     isLaunching = true;
+    playPangSound();
     $("#canAttack").css("display", "none"); // "공격 가능" 숨김
-    pang.css("display", "block").animate({ left: "900px" }, 500, "linear");
+    pang.css("display", "block").animate({ left: "900px" }, 100, "linear");
     crushEnemy();
     pang.animate({ left: "100px" }, 0, function () {
       setTimeout(function () {
         isLaunching = false;
         $("#canAttack").css("display", "block"); // "공격 가능" 표시
-      }, 3000);
+      }, attackInterval);
       pang.css("display", "none");
     });
   }
@@ -183,8 +247,8 @@ $(function () {
     const rect2 = enemy[0].getBoundingClientRect();
     const result = rect1.right <= rect2.left;
 
-    console.log(rect1);
-    console.log(rect2);
+    // console.log(rect1);
+    // console.log(rect2);
 
     return result;
   }
